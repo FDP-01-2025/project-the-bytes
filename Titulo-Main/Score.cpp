4e181a3ed1933e99a803af13dd466d576d3154a2
Andres070212 <00077125@uca.edu.sv>
2025-06-16 12:10:18 -0600

    se traduce texto

---
+++ Titulo-Main/Score.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct Player
+{
+    char nombre[50]; 
+    int puntaje;
+};
+
+int main(){
+string usuario;
+    cout<<"Insert your username: ";
+    cin>>usuario;
+
+    return 0;
+}
