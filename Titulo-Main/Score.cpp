c4212bec802769d1ba82862a1ebb6c9fb299d515
Andres070212 <00077125@uca.edu.sv>
2025-06-16 12:20:07 -0600

    partidas ganadas y perdidas

--- Titulo-Main/Score.cpp
+++ Titulo-Main/Score.cpp
@@ -5,13 +5,19 @@ using namespace std;
 struct Player
 {
     char nombre[50]; 
-    int puntaje;
+    int partidasGanadas;
 };
 
 int main(){
 string usuario;
     cout<<"Insert your username: ";
     cin>>usuario;
+    
+    cout<<"Partidas jugadas: "<<partidas<<endl;
+    cout<<"Partidas perdidas: "<<Ganadas<<endl;
+    cout<<"Partidas ganadas: "<<perdidas<<endl;
+
+    ofstream archivo("player.txt", ios::app);
 
     return 0;
 }
