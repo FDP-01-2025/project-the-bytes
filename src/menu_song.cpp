a04554796b43a0b7cf278276d021c5695f2fdc87
RJMM7 <00148625@uca.edu.sv>
2025-07-06 15:00:26 -0600

    Update file: menu_song

--- src/menu_song.cpp
+++ src/menu_song.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 using namespace std;
+#include <windows.h>
+#include <mmsystem.h>
+#pragma comment(lib, "winmm.lib")
+
+int volume = 10; // It define the initial value of the volume
+
+// Volume from 0 to 100
+void setVolume(int percentage) {
+    DWORD v = (percentage * 0xFFFF) / 100;
+    DWORD finalValue = (v & 0xFFFF) | ((v & 0xFFFF) << 16);
+    waveOutSetVolume(0, finalValue);
+}
