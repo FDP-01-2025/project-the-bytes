67c18df549d84f495ef385e0c9724e7f3e10b3e7
RJMM7 <00148625@uca.edu.sv>
2025-07-06 15:11:51 -0600

    Update: add settings for volume

--- src/menu_song.cpp
+++ src/menu_song.cpp
@@ -12,3 +12,56 @@ void setVolume(int percentage) {
     DWORD finalValue = (v & 0xFFFF) | ((v & 0xFFFF) << 16);
     waveOutSetVolume(0, finalValue);
 }
+
+// Starts the music in the background in a loop
+void playMusic() {
+    PlaySound(TEXT("pirates_song.wav"), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+}
+
+// Stop the music completely
+void stopMusic() {
+    PlaySound(NULL, 0, 0);
+}
+
+// Volume settings menu
+void soundOptions() {
+    int soundOption;
+    bool exitMenu = false;
+
+    while (!exitMenu) {
+        // Options
+        cout << "|======Sound Options======|" << endl;
+        cout << "Current volume: " << volume << "%\n\n";
+        cout << "1. Increase volume (+10%)" << endl;
+        cout << "2. Decrease volume (-10%) << endl";
+        cout << "3. Mute music" << endl;
+        cout << "4. Resume music" << endl;
+        cout << "5. Return to main menu" << endl;
+        cout << "Select an option: ";
+        cin >> soundOption;
+
+        switch (soundOption) {
+            case 1:
+                if (volume < 100) volume += 10; // Increase the volume
+                setVolume(volume); // The new volume is applied
+                break;
+            case 2:
+                if (volume > 0) volume -= 10; // Decrease the volume
+                setVolume(volume); // The new volume is applied
+                break;
+            case 3:
+                stopMusic(); // Stop the music
+                break;
+            case 4:
+                playMusic();
+                setVolume(volume);
+                break;
+            case 5:
+                exitMenu = true;
+                break;
+            default:
+                cout << "Invalid option.\n";
+                Sleep(1000);
+        }
+    }
+}
